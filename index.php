f69ba9ea2b1b29c902acda59d0bc7e0370da5ae2
100405965 <100405965@alumnos.uc3m.es>
2023-05-15 21:11:24 +0200

    Added database query

--- index.php
+++ index.php
@@ -3,6 +3,30 @@
 <title>PHP Test</title>
 </head>
 <body>
-<?php echo '<p>Hello World</p>'; ?>
+<?php
+$conn = new mysqli("mysql-service", "root", "pass", "test");
+// Check connection
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0)
+{
+    // output data of each row
+    echo 'The authors of this practice are:<br>';
+    while($row = $result->fetch_assoc())
+    {
+        echo $row['name']."-".$row['NIA']."<br>";
+    }
+    echo 'This data was queried from the MySQL database.<br>';
+}
+
+$conn->close();
+
+?>
 </body>
 </html>
